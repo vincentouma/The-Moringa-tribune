aa12e4bd2f8bb468ee5fe3da1835edfff6aa9d55
vincentouma <vinceoumah@gmail.com>
2019-08-21 22:06:44 +0300

    update to tuesday content add django admin models and tests

--- news/models.py
+++ news/models.py
@@ -1,7 +1,43 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()
+  
+
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tags(self):
+        self.save()
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def save_Article(self):
+        self.save()
+
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news           
--- news/views.py
+++ news/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Article
+
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+#def welcome(request):
+    #return render(request, 'welcome.html')
 
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html', {"date": date})
-
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
 # View Function to present news from past days
 def past_days_news(request, past_date):
 
     try:
+        editor = Editor.objects.get(email = 'example@gmail.com')
+        print('Editor found')
+    except DoesNotExist:
+        print('Editor was not found')
+
+
+
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
@@ -26,4 +35,5 @@ def past_days_news(request, past_date):
     if date == dt.date.today():
         return redirect(news_of_day)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
