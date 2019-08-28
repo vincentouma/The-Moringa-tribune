f0be8c5dfef24ac318ec62c6a63806473a32dbf3
vincentouma <vinceoumah@gmail.com>
2019-08-28 12:01:02 +0300

    import tynycmce

--- news/models.py
+++ news/models.py
@@ -1,22 +1,11 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length = 10,blank =True)
 
-    def __str__(self):
-        return self.first_name
-
-    class meta:
-        ordering =['name']
-
-
-    def save_editor(self):
-        self.save()
   
 
 
@@ -31,8 +20,8 @@ class tags(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
     article_image = models.ImageField(upload_to = 'articles/',default='DEFAULT VALUE')
@@ -61,4 +50,7 @@ class Article(models.Model):
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news    
-    
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
