23b0f985e38c5abc6fc9f921bf8abd5921c1ff03
vincentouma <vinceoumah@gmail.com>
2019-08-28 12:03:56 +0300

    update base.html

--- news/views.py
+++ news/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm, NewArticleForm
+from .email import send_welcome_email
+from .models import NewsLetterRecipients
+
+
 
 
 # Create your views here.
@@ -11,8 +17,22 @@ from .models import Article
 def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    
 
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 # View Function to present news from past days
 def past_days_news(request, past_date):
 
@@ -48,9 +68,27 @@ def search_results(request):
         return render(request, 'all-news/search.html',{"message":message})    
 
 
-def article(request,article_id):
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('new-article')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
+
+def article(request, article_id):
+
+    
     try:
         article = Article.objects.get(id = article_id)
     except DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
+
--- tribune/settings.py
+++ tribune/settings.py
@@ -11,6 +11,8 @@ import os
 import django_heroku
 import dj_database_url
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,17 +24,26 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from decouple import config, Csv
 MODE=config("MODE", default='dev')
 SECRET_KEY = config('SECRET_KEY')
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
+LOGIN_REDIRECT_URL= ('/')
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'bootstrap3',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
